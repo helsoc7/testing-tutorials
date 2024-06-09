4b78b9e91f0b1ae836d39f1049a1a091a0b78185
Helen Haveloh <h.havelohh@gmail.com>
2024-06-09 13:34:15 +0200

    initial commit

---
+++ api-test/api.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+async function fetchData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error fetching data');
+  }
+}
+
+module.exports = fetchData;
